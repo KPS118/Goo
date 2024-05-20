57f4b25ed34bfe45ec5b2de87d3d6ffc08d39ff6
KPS118 <batenkhyesungoo0804@gmail.com>
2024-05-20 16:32:25 +0800

    chadhaaraa tuslaad uguurui

---
+++ New folder/yesungoo/Diplom/src/utils/data.js
@@ -0,0 +1,74 @@
+import vegan1 from "../img/vegan1.png"
+import vegan2 from "../img/vegan2.png"
+import vegan3 from "../img/vegan3.png"
+import vegan4 from "../img/vegan4.png"
+
+export const heroData = [
+  {
+    id: 1,
+    name: "vegentrain",
+    decp: "apple pineapple",
+    price: "5.27",
+    imageSrc: vegan1,
+  },
+  {
+    id: 2,
+    name: "vegentrain",
+    decp: "carrots onions",
+    price: "10.28",
+    imageSrc: vegan2,
+  },
+  {
+    id: 3,
+    name: "vegentrain",
+    decp: "of all kinds",
+    price: "9.29",
+    imageSrc: vegan3,
+  },
+  {
+    id: 4,
+    name: "vegentrain",
+    decp: "without spices",
+    price: "6.30",
+    imageSrc: vegan4,
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Mushroom tofu soup",
+    urlParamName: "mushroom tofu soup",
+  },
+  {
+    id: 2,
+    name: "Curry",
+    urlParamName: "curry",
+  },
+  {
+    id: 3,
+    name: "Rice",
+    urlParamName: "rice",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    urlParamName: "fish",
+  },
+  {
+    id: 5,
+    name: "Fruits",
+    urlParamName: "fruits",
+  },
+  {
+    id: 6,
+    name: "Icecreams",
+    urlParamName: "icecreams",
+  },
+
+  {
+    id: 7,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
+];
